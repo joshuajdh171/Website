a30897e7d5dfa191753c113473c03d8d90dbdbd6
joshuajdh171 <joshuaalexanderchristian318@gmail.com>
2025-02-01 09:34:48 +0700

    Update Data_frame.py

--- pages/Data_frame.py
+++ pages/Data_frame.py
@@ -7,7 +7,7 @@ maximum_price = data['price'].min()
 minimum_price = data['price'].max()
 selected_category = st.multiselect("Select Category",options=unique_category)
 selected_store = st.multiselect("Select store",options=unique_storename)
-price_point = st.sider("price",min_value=minimum_price,max_value=maximum_price,value=maximum_price)
+price_point = st.slider("price",min_value=minimum_price,max_value=maximum_price,value=maximum_price)
 
                                
 # print(data)
