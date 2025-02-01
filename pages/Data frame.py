0dfdd5f0e4241e87ca744600b339e35f127d8f33
joshuajdh171 <joshuaalexanderchristian318@gmail.com>
2025-02-01 09:27:22 +0700

    Update Data frame.py

--- pages/Data frame.py
+++ pages/Data frame.py
@@ -1,11 +1,13 @@
-import pandas as Gok
+import pandas as pd
 import streamlit as st
-data = Gok.read_excel('./pages/Source.xlsx')
+data = pd.read_excel('./pages/Source.xlsx')
 unique_category = data['Category'].unique()
 unique_storename = data['store'].unique()
+maximum_price = data['price'].min()
+minimum_price = data['price'].max()
 selected_category = st.multiselect("Select Category",options=unique_category)
 selected_store = st.multiselect("Select store",options=unique_storename)
-
+price_point = st.sider("price",min_value=minimum_price,max_value=maximum_price,value+maximum_price)
 
                                
 # print(data)
