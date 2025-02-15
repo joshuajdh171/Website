9a2b98918b2cff4bb5c825069338047e4de82793
joshuajdh171 <joshuaalexanderchristian318@gmail.com>
2025-02-15 09:54:54 +0700

    Update Data_frame.py

--- pages/Data_frame.py
+++ pages/Data_frame.py
@@ -15,11 +15,27 @@ price_point = st.slider("price",min_value=minimum_price,max_value=maximum_price,
 criteria1 = data['Category'].isin(selected_category)
 criteria2 = data['store'].isin(selected_store)
 criteria3 = data['price'] <= price_point
+
+
 join_criteria = (criteria1) & (criteria2) & (criteria3)
+
+
+with st.container(border=True):
+  data = data[join_criteria]
+  data_count = len(data)
+  for i in range(data_count)
+    product_picture = data.iloc[i]['picture']
+
+st.image(product_picture)
+st.dataframe(data,use_container_width=True)
+
+
+'''
 criteria3 = data['price'] > 1200
 criteria4 = (data['price'] >= 1200) & (data['price'] < 2070)
 criteria5 = (criteria1) & (criteria2) & (criteria3)
 data = data[join_criteria]
+'''
 # print(data[criteria5])
 print(data[criteria5].sort_values('price',ascending=True))
 st.dataframe(data)
